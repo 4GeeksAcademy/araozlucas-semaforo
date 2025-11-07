a0056583dab4459d26f18e64a9c4796942bfdfc2
Pimple <araozlucas@gmail.com>
2025-11-07 03:45:36 +0000

    Third commit

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -2,25 +2,31 @@ import React, { useState } from "react";
 
 const Semaforo = () => {
     
-    const [color, setColor] = useState ("red");
+    const [color, setColor] = useState ({ redlight: '',
+                                          yellowlight: '',
+                                          greenlight: ''});
     
-    const alternateColor = () => {
-        
-        if (color === "red") {
-            setColor ("yellow")
-        }
-        else if (color === "yellow") {
-            setColor ("green")
-        };
+    const lightRed = () => {
+        setColor ({redlight: 'glow-red', yellowlight: '', greenlight: ''})
+    };
+
+    const lightYellow = () => {
+        setColor ({redlight: '', yellowlight: 'glow-yellow', greenlight: ''})
+    };
+
+    const lightGreen = () => {
+        setColor ({redlight: '', yellowlight: '', greenlight: 'glow-green'})
     };
     
 
     return (
         <div className="text-center mt-5">
-            <div className="text-center background">
-                <button onClick="" type="button" className="btn btn-danger rounded-circle btn-circle m-1"></button>
-                <button onClick="" type="button" className="btn btn-warning rounded-circle btn-circle m-1"></button>
-                <button onClick="" type="button" className="btn btn-success rounded-circle btn-circle m-1"></button>
+            <div className="text-center">
+                <div className="btn btn-dark m-1">
+                    <button onClick={lightRed} type="button" className={`btn btn-danger rounded-circle btn-circle m-1 ${color.redlight}`}></button>
+                    <button onClick={lightYellow} type="button" className={`btn btn-warning rounded-circle btn-circle m-1 ${color.yellowlight}`}></button>
+                    <button onClick={lightGreen} type="button" className={`btn btn-success rounded-circle btn-circle m-1 ${color.greenlight}`}></button>
+                </div>
             </div>
         </div>
     );
